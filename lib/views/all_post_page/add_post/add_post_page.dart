04c83a7d83b432a387e17f91bb771ba7dcc4b11f
Gonmura <gonmura@GonmuranoMacBook-Pro-2.local>
2025-02-12 12:51:52 +0900

    キーボードより改善

--- lib/views/all_post_page/add_post/add_post_page.dart
+++ lib/views/all_post_page/add_post/add_post_page.dart
@@ -49,120 +49,117 @@ class _AddPostPageState extends State<AddPostPage> {
       );
     }
 
-    return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).unfocus(); // 画面タップ時にキーボードを閉じる
-      },
-      child: Scaffold(
-          appBar: AppBar(title: const Text("投稿追加")),
-          body: SingleChildScrollView(
-            child: Form(
-              key: formKey,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Stack(
-                        // alignment: Alignment(x, y),
-                        children: [
-                          previewWidget,
-                          if (image != null)
-                            Positioned(
-                              top: -20,
-                              right: -20,
-                              child: IconButton(
-                                  onPressed: () async {
-                                    image = null;
-                                    setState(() {});
-                                  },
-                                  icon: Icon(Icons.close,
-                                      size: 50, color: Colors.red)),
-                            )
-                        ]),
-                    MarginBox.mediumHeightMargin,
-                    EditButton(
-                        buttonText: "画像を選択する",
-                        onEditButtonPressed: () {
-                          // File? image;
-                          // final picker =ImagePicker();
-                          getImageFromGallery();
-                        }),
-                    MarginBox.bigWidthMargin,
-                    TextFormField(
-                        controller: postController,
-                        maxLines: 13,
-                        maxLength: 100,
-                        decoration: const InputDecoration(label: Text("投稿文")),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return "テキストを入力してください";
-                          }
-                          return null;
-                        }),
-                    EditButton(
-                      buttonText: "投稿!",
-                      onEditButtonPressed: () async {
-                        if (formKey.currentState!.validate() == false) {
-                          //失敗したときに処理をストップ
-                          return;
+    return Scaffold(
+        appBar: AppBar(title: const Text("投稿追加")),
+        body: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Stack(
+                      // alignment: Alignment(x, y),
+                      children: [
+                        previewWidget,
+                        if (image != null)
+                          Positioned(
+                            top: -20,
+                            right: -20,
+                            child: IconButton(
+                                onPressed: () async {
+                                  image = null;
+                                  setState(() {});
+                                },
+                                icon: Icon(Icons.close,
+                                    size: 50, color: Colors.red)),
+                          )
+                      ]),
+                  MarginBox.mediumHeightMargin,
+                  EditButton(
+                      buttonText: "画像を選択する",
+                      onEditButtonPressed: () {
+                        // File? image;
+                        // final picker =ImagePicker();
+                        getImageFromGallery();
+                      }),
+                  MarginBox.bigWidthMargin,
+                  TextFormField(
+                      controller: postController,
+                      maxLines: 13,
+                      maxLength: 100,
+                      decoration: const InputDecoration(label: Text("投稿文")),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "テキストを入力してください";
                         }
+                        return null;
+                      }),
+                  EditButton(
+                    buttonText: "投稿!",
+                    onEditButtonPressed: () async {
+                      if (formKey.currentState!.validate() == false) {
+                        //失敗したときに処理をストップ
+                        return;
+                      }
 
-                        final String uuid = const Uuid().v4();
+                      FocusScope.of(context).unfocus();
 
-                        //画像があるとき↓
-                        if (image != null) {
-                          // try {
-                          //   final storedImage = await FirebaseStorage.instance
-                          //       .ref("PostsIcon/${user!.uid}")
-                          //       .putFile(image!);
-                          // } catch (e) {
-                          //   print(e);
-                          // }
-                          final storedImage = await FirebaseStorage.instance
-                              .ref("PostsIcon/$uuid")
-                              .putFile(image!);
-                          final String imageUrl =
-                              await storedImage.ref.getDownloadURL();
+                      final String uuid = const Uuid().v4();
 
-                          Posts newPost = Posts(
-                              imageUrl: imageUrl,
-                              postText: postController.text,
-                              userId: FirebaseAuth.instance.currentUser!.uid,
-                              postId: uuid,
-                              createdAt: Timestamp.now(),
-                              updatedAt: Timestamp.now());
-                          await FirebaseFirestore.instance
-                              .collection("posts")
-                              .doc(uuid)
-                              .set(newPost.toJson());
-                          showToast("投稿されました！");
-                          postController.clear();
-                          image = null;
-                          setState(() {});
-                        } else {
-                          Posts newPost = Posts(
-                              imageUrl: "",
-                              postText: postController.text,
-                              userId: FirebaseAuth.instance.currentUser!.uid,
-                              postId: uuid,
-                              createdAt: Timestamp.now(),
-                              updatedAt: Timestamp.now());
-                          await FirebaseFirestore.instance
-                              .collection("posts")
-                              .doc(uuid)
-                              .set(newPost.toJson());
-                          showToast("投稿されました！");
-                          postController.clear();
-                        }
-                      },
-                    )
-                  ],
-                ),
+                      //画像があるとき↓
+                      if (image != null) {
+                        // try {
+                        //   final storedImage = await FirebaseStorage.instance
+                        //       .ref("PostsIcon/${user!.uid}")
+                        //       .putFile(image!);
+                        // } catch (e) {
+                        //   print(e);
+                        // }
+                        final storedImage = await FirebaseStorage.instance
+                            .ref("PostsIcon/$uuid")
+                            .putFile(image!);
+                        final String imageUrl =
+                            await storedImage.ref.getDownloadURL();
+
+                        Posts newPost = Posts(
+                            imageUrl: imageUrl,
+                            postText: postController.text,
+                            userId: FirebaseAuth.instance.currentUser!.uid,
+                            postId: uuid,
+                            createdAt: Timestamp.now(),
+                            updatedAt: Timestamp.now());
+                        await FirebaseFirestore.instance
+                            .collection("posts")
+                            .doc(uuid)
+                            .set(newPost.toJson());
+                        showToast("投稿されました！");
+                        postController.clear();
+                        image = null;
+                        setState(() {});
+                      } else {
+                        Posts newPost = Posts(
+                            imageUrl: "",
+                            postText: postController.text,
+                            userId: FirebaseAuth.instance.currentUser!.uid,
+                            postId: uuid,
+                            createdAt: Timestamp.now(),
+                            updatedAt: Timestamp.now());
+                        await FirebaseFirestore.instance
+                            .collection("posts")
+                            .doc(uuid)
+                            .set(newPost.toJson());
+                        showToast("投稿されました！");
+                        postController.clear();
+                      }
+                    },
+                  )
+                ],
               ),
             ),
-          )),
-    );
+          ),
+        ));
   }
 
   Future getImageFromGallery() async {
