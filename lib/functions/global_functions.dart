29842ed0f24878fef7e232603fcaf5d7256f35a3
Gonmura <gonmura@GonmuranoMacBook-Pro-2.local>
2025-02-20 15:25:11 +0900

    コレクションリファレンス追加

--- lib/functions/global_functions.dart
+++ lib/functions/global_functions.dart
@@ -1,5 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:twitter/data_models/liked_by/likedby.dart';
+import 'package:twitter/data_models/posts/posts.dart';
+import 'package:twitter/data_models/save_posts/saveposts.dart';
+import 'package:twitter/data_models/user_data/userdata.dart';
 
 void showToast(String toastMessage) {
   Fluttertoast.showToast(
@@ -11,3 +16,55 @@ void showToast(String toastMessage) {
       textColor: Colors.white,
       fontSize: 24);
 }
+
+final postsReference =
+    FirebaseFirestore.instance.collection('posts').withConverter<Posts>(
+  // <> ここに変換したい型名をいれます。今回は Post です。
+  fromFirestore: ((snapshot, _) {
+    // 第二引数は使わないのでその場合は _ で不使用であることを分かりやすくしています。
+    return Posts.fromJson(
+        snapshot.data()!); // 先ほど定期着した fromFirestore がここで活躍します。
+  }),
+  toFirestore: ((value, _) {
+    return value.toJson(); // 先ほど適宜した toMap がここで活躍します。
+  }),
+);
+
+final userDataReference =
+    FirebaseFirestore.instance.collection('users').withConverter<UserData>(
+  // <> ここに変換したい型名をいれます。今回は Post です。
+  fromFirestore: ((snapshot, _) {
+    // 第二引数は使わないのでその場合は _ で不使用であることを分かりやすくしています。
+    return UserData.fromJson(
+        snapshot.data()!); // 先ほど定期着した fromFirestore がここで活躍します。
+  }),
+  toFirestore: ((value, _) {
+    return value.toJson(); // 先ほど適宜した toMap がここで活躍します。
+  }),
+);
+
+final savePostsReference =
+    FirebaseFirestore.instance.collection('savePosts').withConverter<SavePosts>(
+  // <> ここに変換したい型名をいれます。今回は Post です。
+  fromFirestore: ((snapshot, _) {
+    // 第二引数は使わないのでその場合は _ で不使用であることを分かりやすくしています。
+    return SavePosts.fromJson(
+        snapshot.data()!); // 先ほど定期着した fromFirestore がここで活躍します。
+  }),
+  toFirestore: ((value, _) {
+    return value.toJson(); // 先ほど適宜した toMap がここで活躍します。
+  }),
+);
+
+final likedByReference =
+    FirebaseFirestore.instance.collection('likedBy').withConverter<LikedBy>(
+  // <> ここに変換したい型名をいれます。今回は Post です。
+  fromFirestore: ((snapshot, _) {
+    // 第二引数は使わないのでその場合は _ で不使用であることを分かりやすくしています。
+    return LikedBy.fromJson(
+        snapshot.data()!); // 先ほど定期着した fromFirestore がここで活躍します。
+  }),
+  toFirestore: ((value, _) {
+    return value.toJson(); // 先ほど適宜した toMap がここで活躍します。
+  }),
+);
