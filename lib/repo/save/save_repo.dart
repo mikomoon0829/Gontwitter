457ef280c80074f62566845a8e9e0a1a79cc7291
mikomoon0829 <miko_moon0829@icloud.com>
2025-02-28 10:31:12 +0900

    saverepoも完成

---
+++ lib/repo/save/save_repo.dart
@@ -0,0 +1,148 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:twitter/config/firebase/firebase_provider.dart';
+import 'package:twitter/config/utils/keys/firebase_key.dart';
+import 'package:twitter/data_models/posts/posts.dart';
+import 'package:twitter/data_models/save_posts/saveposts.dart';
+import 'package:twitter/repo/auth/auth_repo.dart';
+
+part 'save_repo.g.dart';
+
+@riverpod
+//下のに変える
+// class TaskRepo {
+class SaveRepo extends _$SaveRepo {
+//buildを加える
+  @override
+  CollectionReference<SavePosts> build(String userId) {
+    // return FirebaseFirestore.instance
+    //firebaseFirestore.instanceがref.read(firestoreProvider)に変わった！firebase_provider.dartを書くと！
+    return ref
+        .read(firebaseFirestoreProvider)
+        .collection(FirebaseUsersKey.usersCollection)
+        .doc(userId)
+        .collection(FirebaseSavePostsKey.savePostsCollection)
+        .withConverter<SavePosts>(
+          fromFirestore: (snapshot, _) => SavePosts.fromJson(snapshot.data()!),
+          toFirestore: (SavePosts value, _) => value.toJson(),
+        );
+  }
+  // final db = FirebaseFirestore.instance
+  //     .collection(FirebaseTaskKey.taskCollection)
+  //     .withConverter<Task>(
+  //       fromFirestore: (snapshot, _) => Task.fromJson(snapshot.data()!),
+  //       toFirestore: (Task value, _) => value.toJson(),
+  //     );
+
+  //taskIdからドキュメント取得
+  Future<SavePosts> getSavePost(String postId) async {
+    final savePostDoc = await state.doc(postId).get();
+    return savePostDoc.data()!;
+  }
+
+  //FutureでTaskListを取得
+  Future<List<SavePosts>> getSavePosts() async {
+    final snapshot = await state.get();
+    return snapshot.docs.map((doc) => doc.data()).toList();
+  }
+
+////Task型データのリストを扱うとき
+  Stream<List<SavePosts>> watchSavePosts() {
+    // return db.orderBy('createdAt', descending: true).snapshots().map(
+    return state
+        .orderBy(FirebaseSavePostsKey.savedAt, descending: true)
+        .snapshots()
+        .map(
+      //ここでmapとすることで、各要素として<QuerySnapshot<Task>>が入る
+      //（Asyncじゃないから.dataを省略可能（.dataはviewの方で行う！））
+      (QuerySnapshot<SavePosts> snapshot) {
+        return snapshot.docs.map(
+          //それぞれのドキュメントのどきゅめんとsnapshotのリストを返す。と思いきやリストの要素それぞれからTaskを取り出す処理を下で行う
+          (QueryDocumentSnapshot<SavePosts> doc) {
+            return doc.data();
+          },
+        ).toList();
+      },
+    );
+  }
+
+//コレクションに入っているものは全てそのuserIdのものだからいらない
+  // ///自分が作ったTask型データのリストを扱うとき
+  // Stream<List<SavePosts>> watchMySavePosts() {
+  //   // return db.orderBy('createdAt', descending: true).snapshots().map(
+  //   return state
+  //       .orderBy(FirebasePostsKey.createdAt, descending: true)
+  //       .where(FirebasePostsKey.userId,
+  //           isEqualTo: ref.watch(authRepoProvider)!.uid)
+  //       .snapshots()
+  //       .map(
+  //     //ここでmapとすることで、各要素として<QuerySnapshot<Task>>が入る
+  //     //（Asyncじゃないから.dataを省略可能（.dataはviewの方で行う！））
+  //     (QuerySnapshot<Posts> snapshot) {
+  //       return snapshot.docs.map(
+  //         //それぞれのドキュメントのどきゅめんとsnapshotのリストを返す。と思いきやリストの要素それぞれからTaskを取り出す処理を下で行う
+  //         (QueryDocumentSnapshot<Posts> doc) {
+  //           return doc.data();
+  //         },
+  //       ).toList();
+  //     },
+  //   );
+// }
+
+//一件のTask型データを扱うとき
+  Stream<SavePosts> watchSavePost(String postId) {
+    // return db.doc('docId').snapshots().map(
+    return state.doc(postId).snapshots().map(
+      (DocumentSnapshot<SavePosts> snapshot) {
+        return snapshot.data()!; //.data()でDocumentSnapshotを外せる
+      },
+    );
+  }
+
+//ドキュメント追加
+  Future<void> addSavePost(SavePosts addPostData) async {
+    // await db.doc(addTaskData.taskId).set(addTaskData);
+    await state.doc(addPostData.postId).set(addPostData);
+  }
+
+//ドキュメント削除
+  Future<void> deletePost(String postId) async {
+    await state.doc(postId).delete();
+  }
+
+//保存にupdateもなにもないやろ
+// //ドキュメント更新
+//   Future<void> updatePost(SavePosts updatePostData) async {
+//     await state.doc(updatePostData.postId).update(updatePostData.toJson());
+//   }
+}
+
+// //watchTaskのみを切り出したプロバイダを作る
+@riverpod
+Stream<SavePosts> savePostStream(Ref ref, String postId, String userId) {
+  return ref.watch(saveRepoProvider(userId).notifier).watchSavePost(postId);
+  //snapshotでコレクションを監視したものの一覧を降順にならべたものが状態であるbasicProvider
+  //その状態を返すということはstream型を返すプロバイダだからwhen使える！
+  //これ切り出してなかったら、TaskRepoプロバイダは単に固定値を返すものやからwhen使えないよね！
+}
+
+// //watchTasksのみを切り出したプロバイダを作る
+@riverpod
+Stream<List<SavePosts>> savePostsStream(Ref ref, String userId) {
+  return ref.watch(saveRepoProvider(userId).notifier).watchSavePosts();
+  //snapshotでコレクションを監視したものの一覧を降順にならべたものが状態であるbasicProvider
+  //その状態を返すということはstream型を返すプロバイダだからwhen使える！
+  //これ切り出してなかったら、TaskRepoプロバイダは単に固定値を返すものやからwhen使えないよね！
+}
+
+//コレクションに入っているものは全てそのuserIdのものだからいらない
+// //watchTasksのみを切り出したプロバイダを作る
+// @riverpod
+// Stream<List<SavePosts>> myPostsStream(Ref ref,String userId) {
+//   return ref.watch(saveRepoProvider(userId).notifier).watchMySavePosts();
+//   //snapshotでコレクションを監視したものの一覧を降順にならべたものが状態であるbasicProvider
+//   //その状態を返すということはstream型を返すプロバイダだからwhen使える！
+//   //これ切り出してなかったら、TaskRepoプロバイダは単に固定値を返すものやからwhen使えないよね！
+// }
---
+++ lib/repo/save/save_repo.g.dart
@@ -0,0 +1,454 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'save_repo.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$savePostStreamHash() => r'7eb26613f95bac37581a6b290f3710ade5cf5627';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+/// See also [savePostStream].
+@ProviderFor(savePostStream)
+const savePostStreamProvider = SavePostStreamFamily();
+
+/// See also [savePostStream].
+class SavePostStreamFamily extends Family<AsyncValue<SavePosts>> {
+  /// See also [savePostStream].
+  const SavePostStreamFamily();
+
+  /// See also [savePostStream].
+  SavePostStreamProvider call(
+    String postId,
+    String userId,
+  ) {
+    return SavePostStreamProvider(
+      postId,
+      userId,
+    );
+  }
+
+  @override
+  SavePostStreamProvider getProviderOverride(
+    covariant SavePostStreamProvider provider,
+  ) {
+    return call(
+      provider.postId,
+      provider.userId,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'savePostStreamProvider';
+}
+
+/// See also [savePostStream].
+class SavePostStreamProvider extends AutoDisposeStreamProvider<SavePosts> {
+  /// See also [savePostStream].
+  SavePostStreamProvider(
+    String postId,
+    String userId,
+  ) : this._internal(
+          (ref) => savePostStream(
+            ref as SavePostStreamRef,
+            postId,
+            userId,
+          ),
+          from: savePostStreamProvider,
+          name: r'savePostStreamProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$savePostStreamHash,
+          dependencies: SavePostStreamFamily._dependencies,
+          allTransitiveDependencies:
+              SavePostStreamFamily._allTransitiveDependencies,
+          postId: postId,
+          userId: userId,
+        );
+
+  SavePostStreamProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.postId,
+    required this.userId,
+  }) : super.internal();
+
+  final String postId;
+  final String userId;
+
+  @override
+  Override overrideWith(
+    Stream<SavePosts> Function(SavePostStreamRef provider) create,
+  ) {
+    return ProviderOverride(
+      origin: this,
+      override: SavePostStreamProvider._internal(
+        (ref) => create(ref as SavePostStreamRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        postId: postId,
+        userId: userId,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeStreamProviderElement<SavePosts> createElement() {
+    return _SavePostStreamProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is SavePostStreamProvider &&
+        other.postId == postId &&
+        other.userId == userId;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, postId.hashCode);
+    hash = _SystemHash.combine(hash, userId.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin SavePostStreamRef on AutoDisposeStreamProviderRef<SavePosts> {
+  /// The parameter `postId` of this provider.
+  String get postId;
+
+  /// The parameter `userId` of this provider.
+  String get userId;
+}
+
+class _SavePostStreamProviderElement
+    extends AutoDisposeStreamProviderElement<SavePosts> with SavePostStreamRef {
+  _SavePostStreamProviderElement(super.provider);
+
+  @override
+  String get postId => (origin as SavePostStreamProvider).postId;
+  @override
+  String get userId => (origin as SavePostStreamProvider).userId;
+}
+
+String _$savePostsStreamHash() => r'7860583f6e424a8441d352c3ddf1620e9e1cb22f';
+
+/// See also [savePostsStream].
+@ProviderFor(savePostsStream)
+const savePostsStreamProvider = SavePostsStreamFamily();
+
+/// See also [savePostsStream].
+class SavePostsStreamFamily extends Family<AsyncValue<List<SavePosts>>> {
+  /// See also [savePostsStream].
+  const SavePostsStreamFamily();
+
+  /// See also [savePostsStream].
+  SavePostsStreamProvider call(
+    String userId,
+  ) {
+    return SavePostsStreamProvider(
+      userId,
+    );
+  }
+
+  @override
+  SavePostsStreamProvider getProviderOverride(
+    covariant SavePostsStreamProvider provider,
+  ) {
+    return call(
+      provider.userId,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'savePostsStreamProvider';
+}
+
+/// See also [savePostsStream].
+class SavePostsStreamProvider
+    extends AutoDisposeStreamProvider<List<SavePosts>> {
+  /// See also [savePostsStream].
+  SavePostsStreamProvider(
+    String userId,
+  ) : this._internal(
+          (ref) => savePostsStream(
+            ref as SavePostsStreamRef,
+            userId,
+          ),
+          from: savePostsStreamProvider,
+          name: r'savePostsStreamProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$savePostsStreamHash,
+          dependencies: SavePostsStreamFamily._dependencies,
+          allTransitiveDependencies:
+              SavePostsStreamFamily._allTransitiveDependencies,
+          userId: userId,
+        );
+
+  SavePostsStreamProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.userId,
+  }) : super.internal();
+
+  final String userId;
+
+  @override
+  Override overrideWith(
+    Stream<List<SavePosts>> Function(SavePostsStreamRef provider) create,
+  ) {
+    return ProviderOverride(
+      origin: this,
+      override: SavePostsStreamProvider._internal(
+        (ref) => create(ref as SavePostsStreamRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        userId: userId,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeStreamProviderElement<List<SavePosts>> createElement() {
+    return _SavePostsStreamProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is SavePostsStreamProvider && other.userId == userId;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, userId.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin SavePostsStreamRef on AutoDisposeStreamProviderRef<List<SavePosts>> {
+  /// The parameter `userId` of this provider.
+  String get userId;
+}
+
+class _SavePostsStreamProviderElement
+    extends AutoDisposeStreamProviderElement<List<SavePosts>>
+    with SavePostsStreamRef {
+  _SavePostsStreamProviderElement(super.provider);
+
+  @override
+  String get userId => (origin as SavePostsStreamProvider).userId;
+}
+
+String _$saveRepoHash() => r'0cf3e1c10668c5bc41ccd30541ec7b2ce14d50e2';
+
+abstract class _$SaveRepo
+    extends BuildlessAutoDisposeNotifier<CollectionReference<SavePosts>> {
+  late final String userId;
+
+  CollectionReference<SavePosts> build(
+    String userId,
+  );
+}
+
+/// See also [SaveRepo].
+@ProviderFor(SaveRepo)
+const saveRepoProvider = SaveRepoFamily();
+
+/// See also [SaveRepo].
+class SaveRepoFamily extends Family<CollectionReference<SavePosts>> {
+  /// See also [SaveRepo].
+  const SaveRepoFamily();
+
+  /// See also [SaveRepo].
+  SaveRepoProvider call(
+    String userId,
+  ) {
+    return SaveRepoProvider(
+      userId,
+    );
+  }
+
+  @override
+  SaveRepoProvider getProviderOverride(
+    covariant SaveRepoProvider provider,
+  ) {
+    return call(
+      provider.userId,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'saveRepoProvider';
+}
+
+/// See also [SaveRepo].
+class SaveRepoProvider extends AutoDisposeNotifierProviderImpl<SaveRepo,
+    CollectionReference<SavePosts>> {
+  /// See also [SaveRepo].
+  SaveRepoProvider(
+    String userId,
+  ) : this._internal(
+          () => SaveRepo()..userId = userId,
+          from: saveRepoProvider,
+          name: r'saveRepoProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$saveRepoHash,
+          dependencies: SaveRepoFamily._dependencies,
+          allTransitiveDependencies: SaveRepoFamily._allTransitiveDependencies,
+          userId: userId,
+        );
+
+  SaveRepoProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.userId,
+  }) : super.internal();
+
+  final String userId;
+
+  @override
+  CollectionReference<SavePosts> runNotifierBuild(
+    covariant SaveRepo notifier,
+  ) {
+    return notifier.build(
+      userId,
+    );
+  }
+
+  @override
+  Override overrideWith(SaveRepo Function() create) {
+    return ProviderOverride(
+      origin: this,
+      override: SaveRepoProvider._internal(
+        () => create()..userId = userId,
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        userId: userId,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeNotifierProviderElement<SaveRepo, CollectionReference<SavePosts>>
+      createElement() {
+    return _SaveRepoProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is SaveRepoProvider && other.userId == userId;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, userId.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin SaveRepoRef
+    on AutoDisposeNotifierProviderRef<CollectionReference<SavePosts>> {
+  /// The parameter `userId` of this provider.
+  String get userId;
+}
+
+class _SaveRepoProviderElement extends AutoDisposeNotifierProviderElement<
+    SaveRepo, CollectionReference<SavePosts>> with SaveRepoRef {
+  _SaveRepoProviderElement(super.provider);
+
+  @override
+  String get userId => (origin as SaveRepoProvider).userId;
+}
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
